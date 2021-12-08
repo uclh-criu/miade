8f7733b77ca96208e65fead4d163ba1e1c1ff874
James Brandreth <james.brandreth@gmail.com>
2021-12-08 12:10:47 +0000

    Update tests

--- src/nlp_engine_core/concept.py
+++ src/nlp_engine_core/concept.py
@@ -7,3 +7,6 @@ class Concept(object):
 
     def __str__(self):
         return str(self.__dict__)
+
+    def __eq__(self, other):
+        return (self.__dict__ == other.__dict__)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,59 +1,12 @@
 import pytest
 
-import nlp_engine_core
+from nlp_engine_core.core import NoteProcessor
+from nlp_engine_core.concept import Concept
 
 
 def test_core(model_filepath, test_note):
-    processor = nlp_engine_core.core.NoteProcessor(model_filepath)
-    extracted_entities = processor.annotator.get_entities(test_note)
-    assert extracted_entities == {
-        "entities": {
-            0: {
-                "pretty_name": "Diagnosis",
-                "cui": "C0011900",
-                "type_ids": ["T060"],
-                "types": ["Diagnostic Procedure"],
-                "source_value": "diagnosed",
-                "detected_name": "diagnosed",
-                "acc": 0.391300890979873,
-                "context_similarity": 0.391300890979873,
-                "start": 7,
-                "end": 16,
-                "icd10": [],
-                "ontologies": [],
-                "snomed": [],
-                "id": 0,
-                "meta_anns": {
-                    "Status": {
-                        "value": "Affirmed",
-                        "confidence": 0.9999933838844299,
-                        "name": "Status",
-                    }
-                },
-            },
-            2: {
-                "pretty_name": "Kidney Failure",
-                "cui": "C0035078",
-                "type_ids": ["T047"],
-                "types": ["Disease or Syndrome"],
-                "source_value": "kidney failure",
-                "detected_name": "kidney~failure",
-                "acc": 1.0,
-                "context_similarity": 1.0,
-                "start": 22,
-                "end": 36,
-                "icd10": [],
-                "ontologies": [],
-                "snomed": [],
-                "id": 2,
-                "meta_anns": {
-                    "Status": {
-                        "value": "Affirmed",
-                        "confidence": 0.9999961853027344,
-                        "name": "Status",
-                    }
-                },
-            },
-        },
-        "tokens": [],
-    }
+    processor = NoteProcessor(model_filepath)
+    assert processor.process(test_note) == [
+        Concept(id='C0011900', name='Diagnosis'),
+        Concept(id='C0035078', name='Kidney Failure')
+    ]
