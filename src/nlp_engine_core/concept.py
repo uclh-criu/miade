d025ae0491bc88b9b0fe26737df64aab37f4b864
James Brandreth <james.brandreth@gmail.com>
2021-12-08 11:40:42 +0000

    returns concepts

--- src/nlp_engine_core/concept.py
+++ src/nlp_engine_core/concept.py
@@ -1,2 +1,9 @@
 class Concept(object):
     """docstring for Concept."""
+
+    def __init__(self, id: str, name: str):
+        self.name = name
+        self.id = id
+
+    def __str__(self):
+        return str(self.__dict__)
--- src/nlp_engine_core/core.py
+++ src/nlp_engine_core/core.py
@@ -21,4 +21,8 @@ class NoteProcessor:
         self.annotator = CAT.load_model_pack(model_pack_filepath)
 
     def process(self, note: Note, patient_data: Optional[List[Concept]] = None) -> List[Concept]:
-        return self.annotator.get_entities(note)
+        concepts = []
+        entities = self.annotator.get_entities(note)
+        for entity in entities['entities'].values():
+            concepts.append(Concept(id=entity['cui'], name=entity['pretty_name']))
+        return concepts
