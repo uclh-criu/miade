2d3c995c8768ef9c0826b6997860baa4891a3fc1
jenniferajiang <jennigato95@gmail.com>
2021-12-10 16:32:34 +0000

    added vocab builder

--- src/model_builders/vocabbuilder.py
+++ src/model_builders/vocabbuilder.py
@@ -1,7 +1,24 @@
 from pathlib import Path
+from typing import Optional
+
 from medcat.vocab import Vocab
 
 
 class VocabBuilder(object):
-    def __init__(self, data_path: Path):
-        self.data_path = data_path
+    """Builds vocab corpus"""
+    def __init__(self, vocab_path: Optional[Path] = None):
+        if vocab_path is not None:
+            self.vocab = Vocab.load(vocab_path)
+        else:
+            self.vocab = Vocab()
+
+    def add_new_corpus(self, data_path: Path) -> None:
+        self.vocab.add_words(str(data_path), replace=True)
+
+    def add_to_existing_corpus(self, data_path: Path) -> None:
+        self.vocab.add_words(str(data_path), replace=False)
+
+    def create_vocab(self) -> None:
+        self.vocab.make_unigram_table()
+        self.vocab.save("vocab.dat")
+
