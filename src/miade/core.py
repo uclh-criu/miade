5d49559c2f18499bcd007137348795300f8b40bb
James Brandreth <james.brandreth@gmail.com>
2022-01-06 13:39:34 +0000

    Add multi-model support

--- src/miade/core.py
+++ src/miade/core.py
@@ -17,15 +17,27 @@ from .concept import Concept
 class NoteProcessor:
     """docstring for NoteProcessor."""
 
-    def __init__(self, model_pack_filepath: Path):
-        self.annotator = CAT.load_model_pack(model_pack_filepath)
+    def __init__(self, model_directory: Path):
+        self.annotators = [
+                CAT.load_model_pack(model_pack_filepath)
+            for
+                model_pack_filepath
+            in
+                model_directory.glob('*.zip')
+        ]
+        print(self.annotators)
 
     def process(self, note: Note, patient_data: Optional[List[Concept]] = None) -> List[Concept]:
-        return [
-                Concept(id=entity['cui'], name=entity['pretty_name'])
-            for
-                entity
-            in
-                self.annotator.get_entities(note)['entities'].values()
-            if 'Disease or Syndrome' in entity['types']
-        ]
+
+        concepts: List[Concept] = []
+
+        for entity in self.annotators[0].get_entities(note)['entities'].values():
+            print(type(entity))
+            print(entity)
+
+        for annotator in self.annotators:
+            for entity in annotator.get_entities(note)['entities'].values():
+                if 'Disease or Syndrome' in entity['types'] or 'Pharmacologic Substance' in entity['types']:
+                        concepts.append(Concept(id=entity['cui'], name=entity['pretty_name']))
+
+        return concepts
