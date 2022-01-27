b893433f500d809df6348313348de599d6275738
James Brandreth <james.brandreth@gmail.com>
2022-01-27 14:52:44 +0000

    :white_check_mark: CDB Test

--- tests/test_cdbbuilder.py
+++ tests/test_cdbbuilder.py
@@ -6,7 +6,17 @@ from model_builders.cdbbuilder import CDBBuilder
 
 
 def test_cdbbuilder(snomed_data_path, fdb_data_path, cdb_csv_paths):
-    cdb_builder = CDBBuilder(snomed_data_path=snomed_data_path, fdb_data_path=fdb_data_path)
-    cdb_builder.preprocess_snomed(output_dir=Path('./tests/data/'))
-    cdb_builder.preprocess_fdb(output_dir=Path('./tests/data/'))
-    cdb_builder.create_cdb(cdb_csv_paths)
+    cdb_builder = CDBBuilder(
+        snomed_data_path=snomed_data_path, fdb_data_path=fdb_data_path
+    )
+    cdb_builder.preprocess_snomed(output_dir=Path("./tests/data/"))
+    cdb_builder.preprocess_fdb(output_dir=Path("./tests/data/"))
+    cdb = cdb_builder.create_cdb(cdb_csv_paths)
+    assert cdb.cui2names == {
+        "1": {"covid"},
+        "3": {"liver~failure"},
+        "4": {"silodosin~8mg~capsules", "silodosin~8mg~capsule"},
+        "10": {"paracetamol"},
+        "20": {"ibruprofen"},
+        "30": {"co~codamol"},
+    }
