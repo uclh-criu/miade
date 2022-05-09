1ac37239ecd64f329621bb613264a49a8cc54ba3
James Brandreth <james.brandreth@gmail.com>
2022-05-09 17:11:08 +0100

    load a relationship file

---
+++ src/miade/snomed/relationships.py
@@ -0,0 +1,52 @@
+from enum import Enum
+from pathlib import Path
+
+from pandas import read_csv
+
+
+class Direction(Enum):
+    FORWARD = 'forward'
+    REVERSE = 'reverse'
+
+
+class ConceptGraph:
+
+    def __init__(self):
+        self.dict = {}
+
+    def _add_relationship(self, source: str, dest: str, type: str, direction: Direction):
+        if not self.dict.get(source):
+            self.dict[source] = {
+                Direction.FORWARD.value: {},
+                Direction.REVERSE.value: {}
+            }
+        if not self.dict[source][direction.value].get(type):
+            self.dict[source][direction.value][type] = set()
+        self.dict[source][direction.value][type].add(dest)
+
+    def _add_relationship_pair(self, source: str, dest: str, type: str):
+        self._add_relationship(source, dest, type, Direction.FORWARD)
+        self._add_relationship(dest, source, type, Direction.REVERSE)
+
+    @classmethod
+    def from_snomed_snapshot(cls, filename: Path):
+        graph = cls()
+        relationships = read_csv(filename, sep="\t")
+        for _, relationship in relationships.iterrows():
+            concept = relationship['sourceId']
+            relative = relationship['destinationId']
+            relationship_type = relationship['typeId']
+            graph._add_relationship_pair(concept, relative, relationship_type)
+
+        return graph
+
+    def __str__(self):
+        string = ""
+        for concept, relationships in self.dict.items():
+            string += f"{concept} -> {relationships}\n"
+        return string
+
+
+if __name__ == "__main__":
+    graph = ConceptGraph.from_snomed_snapshot("data/snomed/SnomedCT_UKClinicalRF2_PRODUCTION_20211124T000001Z/Snapshot/Terminology/sct2_Relationship_UKCLSnapshot_GB1000000_20211124.txt")
+    print(graph)
