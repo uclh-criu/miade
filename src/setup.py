cd97e062547ecf88d7e7056d24d4a5b561e24cf5
James Brandreth <james.brandreth@gmail.com>
2022-05-11 12:36:18 +0100

    Typer CLI

--- src/setup.py
+++ src/setup.py
@@ -14,5 +14,10 @@ setup(
         "medcat==1.2.8",
         "spacy==3.1.0",
         "typing==3.7.4.3",
+        "typer",
+        "pathlib"
     ],
+    scripts=[
+        "scripts/miade"
+    ]
 )
