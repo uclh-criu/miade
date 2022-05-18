cd8f43be8430af510babbc8e9fa45b78c0fed10a
James Brandreth <james.brandreth@gmail.com>
2022-05-18 10:54:23 +0100

    v0.2

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="miade",
-    version="0.1",
+    version="0.2",
     description="",
     packages=find_packages(),
     include_package_data=True,
