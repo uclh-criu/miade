dd98b5ce4766d87e5e519c59078f8150a068d425
jenniferajiang <jennigato95@gmail.com>
2022-10-11 12:05:11 +0100

    ditched list comprehension for iteration with logs so that it's easier to debug

--- src/miade/conceptfilter.py
+++ src/miade/conceptfilter.py
@@ -1,5 +1,10 @@
+import logging
+
 from typing import List, Optional
 from .concept import Concept, Category
+
+
+log = logging.getLogger(__name__)
 
 
 class ConceptFilter:
@@ -14,16 +19,23 @@ class ConceptFilter:
     def deduplicate(self) -> List[Concept]:
         self.extracted_concepts = sorted(set(self.extracted_concepts))
         if self.record_concepts is not None:
-            return [
-                concept for concept in self.extracted_concepts
-                if concept.id not in [
-                    concept.id for concept in self.record_concepts
-                ] and concept.name not in [
-                       concept.name for concept in self.record_concepts if concept.category == Category.ALLERGY
-                   ]
-            ]
-        else:
-            return self.extracted_concepts
+            filtered_concepts = []
+            for extracted_concept in self.extracted_concepts:
+                if extracted_concept.id in [record_concept.id for record_concept in self.record_concepts]:
+                    # medication code is snomed though - need lookup?
+                    log.debug(f"Filtered duplicate problem/medication {extracted_concept}")
+                    continue
+                if extracted_concept.category == Category.ALLERGY and extracted_concept.name in [
+                    record_concept.name for record_concept in self.record_concepts
+                    if record_concept.category == Category.ALLERGY
+                ]:
+                    log.debug(f"Filtered duplicate allergy {extracted_concept}")
+                    continue
+
+                filtered_concepts.append(extracted_concept)
+            self.extracted_concepts = filtered_concepts
+
+        return self.extracted_concepts
 
     def disambiguate_meds_allergen(self) -> List[Concept]:
         pass
