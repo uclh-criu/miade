f5c5f6c01bb8e92e18c4d13a2d78ddae1ab13d50
Jennifer Jiang <37081323+jenniferajiang@users.noreply.github.com>
2024-01-09 16:00:49 +0000

    Make problem list limit configurable (#110)
    * Made structured problem list limit configurable
    
    * :recycle: Refactored paragraph chunking and updated tests
    
    * :recycle: Made Annotator abstract class for readability
    
    * Prefix numbering is also configurable

--- src/miade/utils/annotatorconfig.py
+++ src/miade/utils/annotatorconfig.py
@@ -5,4 +5,6 @@ from typing import List, Optional
 class AnnotatorConfig(BaseModel):
     lookup_data_path: Optional[str] = "./lookup_data/"
     negation_detection: Optional[str] = "negex"
+    structured_list_limit: Optional[int] = 100
     disable: List[str] = []
+    add_numbering: bool = False
