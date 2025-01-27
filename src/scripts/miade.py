55f3c31812cca7327e743cba472f79e8a8b1f1c6
James Brandreth <james.brandreth@gmail.com>
2025-01-27 15:34:51 +0000

    set nclasses in metamodels when training

--- src/scripts/miade.py
+++ src/scripts/miade.py
@@ -767,13 +767,9 @@ def make(config_filepath: Path, temp_dir: Path = Path("./.temp"), output: Path =
             labels = list(label_counts.keys())
             total_label_count = sum(label_counts.values())
 
-            if meta_spec.balance_weights:
-                meta_model.config.train["class_weights"] = []
-                i = 0
-                for label in labels:
-                    meta_model.config.train["class_weights"].append(1 - (label_counts[label] / total_label_count))
-                    meta_model.config.general["category_value2id"][label] = i
-                    i += 1
+            meta_model.config.general["category_value2id"] = dict(zip(labels, range(0,len(labels))))
+            meta_model.config.train["class_weights"] = [1 - (label_counts[label] / total_label_count) for label in labels]
+            meta_model.config.model.nclasses = len(labels)
 
             log.info(
                 f"Starting MetaCAT training for {meta_spec.config.general['category_name']} for {meta_spec.config.train.nepochs} epoch(s) "
